2714dec65b269e62921d7e0bb3e101c5a1969a98
Robert Zamora <zamora.rr@gmail.com>
2021-08-08 23:03:42 -0400

    update test

--- tests/testthat/test-scanner.r
+++ tests/testthat/test-scanner.r
@@ -1,9 +1,9 @@
 test_that("scan_tokens works on valid expression", {
   actual <- scan_tokens("3 + 4")
   expected <- list(
-    token(token_type$NUMBER, "3", "3", line = 1L),
+    token(token_type$NUMBER, "3", 3, line = 1L),
     token(token_type$PLUS, "+", "", line = 1L),
-    token(token_type$NUMBER, "4", "4", line = 1L),
+    token(token_type$NUMBER, "4", 4, line = 1L),
     token(token_type$EOF, "", "", line = 1L)
   )
 
